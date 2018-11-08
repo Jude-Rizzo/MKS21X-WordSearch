36812663d607b5fdd9dd7bc1abcbfd532f1a19b1
Jude Rizzo <jrizzo00@stuy.edu>
2018-11-07 22:52:12 -0500

    done

--- WordSearch.java
+++ WordSearch.java
@@ -13,22 +13,21 @@ public class WordSearch{
 
 
 
-    public WordSearch(int rows,int cols){
+    public WordSearch(int rows, int cols){
+      if(cols < 0 || rows < 0)
+        throw new IllegalArgumentException("No");
       col = cols;
       row = rows;
-      for(int i = 0; i < rows; i++){
-        for(int j = 0; j<cols; j++){
-          data[i][j] = '_';
-        }
-      }
+      data = new char[rows][cols];
+      this.clear();
 
 
     }
 
     /**Set all values in the WordSearch to underscores'_'*/
     private void clear(){
-      for(int i = 0; i < data.length; i++){
-        for(int j = 0; j<data.length; j++){
+      for(int i = 0; i < row; i++){
+        for(int j = 0; j<col; j++){
           data[i][j] = '_';
         }
       }
@@ -39,16 +38,16 @@ public class WordSearch{
      *@return a String with each character separated by spaces, and rows
      *separated by newlines.
      */
-    public String toString(){
-      String ans = "";
-      for(int i = 0; i < data.length; i++){
-        if(i > 0)
-          ans+="/n";
-        for(int j = 0; j<data.length; j++){
-          ans += data[i][j];
+     public String toString(){
+          String output = "";
+          for (int i = 0; i < data.length; i++){
+            for (int j = 0; j < data[i].length; j++){
+              output += data[i][j] + " ";
+            }
+            output += "\n";
+          }
+          return output;
         }
-      } return ans;
-    }
 
 
 
@@ -63,33 +62,46 @@ public class WordSearch{
      * or there are overlapping letters that do not match, then false is returned
      * and the board is NOT modified.
      */
-     public boolean addWordHorizontal(String word,int rows, int cols){
-      if(data[rows].length - cols < word.length()){
-        return false;
-      }
-      for(int i = cols; i < cols + word.length(); i++){
-        data[rows][i] = word.charAt(i);
-      } return true;
-    }
+     public boolean addWordHorizontal(String word,int row, int col){
+     if (word.length() > data[row].length - col) return false;
+     int col2 = col;
+     for (int i = 0; i < word.length(); i++){
+       if (data[row][col2] != '_' && data[row][col2] != word.charAt(i)){
+         return false;
+       }
+       col2++;
+     }
+     for (int i = 0; i < word.length(); i++){
+       data[row][col] = word.charAt(i);
+       col++;
+     }
+     return true;
+     }
 
-   /**Attempts to add a given word to the specified position of the WordGrid.
-     *The word is added from top to bottom, must fit on the WordGrid, and must
-     *have a corresponding letter to match any letters that it overlaps.
-     *
-     *@param word is any text to be added to the word grid.
-     *@param row is the vertical locaiton of where you want the word to start.
-     *@param col is the horizontal location of where you want the word to start.
-     *@return true when the word is added successfully. When the word doesn't fit,
-     *or there are overlapping letters that do not match, then false is returned.
-     *and the board is NOT modified.
-     */
-    public boolean addWordVertical(String word,int rows, int cols){
-      if(data[cols].length - rows < word.length()){
-        return false;
-      }
-      for(int i = rows; i < rows + word.length(); i++){
-        data[i][rows] = word.charAt(i);
-      } return true;
-    }
-
-}
+    /**Attempts to add a given word to the specified position of the WordGrid.
+      *The word is added from top to bottom, must fit on the WordGrid, and must
+      *have a corresponding letter to match any letters that it overlaps.
+      *
+      *@param word is any text to be added to the word grid.
+      *@param row is the vertical locaiton of where you want the word to start.
+      *@param col is the horizontal location of where you want the word to start.
+      *@return true when the word is added successfully. When the word doesn't fit,
+      *or there are overlapping letters that do not match, then false is returned.
+      *and the board is NOT modified.
+      */
+     public boolean addWordVertical(String word,int row, int col){
+     if (word.length() > data.length - row) return false;
+     int row2 = row;
+     for (int i = 0; i < word.length(); i++){
+       if (data[row2][col] != '_' && data[row2][col] != word.charAt(i)){
+         return false;
+       }
+       row2++;
+     }
+     for (int i = 0; i < word.length(); i++){
+       data[row][col] = word.charAt(i);
+       row++;
+     }
+     return true;
+     }
+ }
